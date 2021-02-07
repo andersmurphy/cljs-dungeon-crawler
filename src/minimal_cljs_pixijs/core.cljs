3794250ae3a54935989774b19a28fb0273775e7a
andersmurphy <andersmurphy@gmail.com>
2021-02-07 20:58:28 +0000

    Spinning Egg

--- src/minimal_cljs_pixijs/core.cljs
+++ src/minimal_cljs_pixijs/core.cljs
@@ -1,3 +1,19 @@
-(ns minimal-cljs-pixijs.core)
+(ns minimal-cljs-pixijs.core
+  (:require [pixi.js :as px]))
 
-(js/alert "Hello there world!")
+(def app (px/Application. #js {:backgroundColor 0x1099bb}))
+(.appendChild (. js/document -body) (. app -view))
+
+(def egg (. px/Sprite from "assets/eggHead.png"))
+(.set (.-anchor egg) 0.5)
+(set! (.-x egg) (/ (.-width (.-screen app)) 2))
+(set! (.-y egg) (/ (.-height (.-screen app)) 2))
+(.addChild (.-stage app) egg)
+
+(defn rotate-egg [delta]
+  (set! (.-rotation egg)
+        (+ (.-rotation egg)
+           (* 0.1 delta))))
+
+(.add (.-ticker app) rotate-egg)
+(.remove (.-ticker app) rotate-egg)
