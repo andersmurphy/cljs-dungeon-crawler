ecf1f5347196d83a420f3bc00e2fc358f7790184
andersmurphy <andersmurphy@gmail.com>
2021-02-14 22:19:42 +0000

    Use pixi loader

--- src/minimal_cljs_pixijs/core.cljs
+++ src/minimal_cljs_pixijs/core.cljs
@@ -1,20 +1,26 @@
 (ns minimal-cljs-pixijs.core
   (:require [pixi.js :as px]))
 
+;; (set! *warn-on-infer* true)
+
 (def ^js app (px/Application. #js {:backgroundColor 0x1099bb}))
+(-> js/document .-body (.appendChild (. app -view)))
 
-(.. js/document -body (appendChild (. app -view)))
+(defn texture-for [resource-name]
+  (let [^js r (-> app .-loader .-resources (aget resource-name))]
+    (.-texture r)))
 
-(def egg (. px/Sprite from "assets/eggHead.png"))
-(.. egg -anchor (set 0.5))
-(set! (.-x egg) (/ (.. app -screen -width) 2))
-(set! (.-y egg) (/ (.. app -screen -height) 2))
-
-(.. app -stage (addChild egg))
-
-(defn rotate-egg [delta]
+(defn rotate-egg [^js egg delta]
   (set! (.-rotation egg)
         (+ (.-rotation egg)
            (* 0.1 delta))))
-(.. app -ticker (add rotate-egg))
-;; (.. app -ticker (remove rotate-egg))
+
+(defn setup []
+  (let [^js egg (px/Sprite. (texture-for "assets/eggHead.png"))]
+    (-> egg .-anchor (.set 0.5))
+    (set! (.-x egg) (/ (-> app .-screen .-width) 2))
+    (set! (.-y egg) (/ (-> app .-screen .-height) 2))
+    (-> app .-ticker (.add (partial rotate-egg egg)))
+    (-> app .-stage (.addChild egg))))
+
+(-> app .-loader (.add "assets/eggHead.png") (.load setup))
